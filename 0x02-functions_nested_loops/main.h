8d7fd067207c535a6517f19fc921617a62f75259
Rannzy <ernaran6@gmail.com>
2022-09-14 05:02:07 -0700

    Putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,157 +1,18 @@
-Your storage is full. … You can't upload new files to Drive and may not be able to send or receive emails in Gmail.Learn more
-
-main.h.txt
-
-Yesterday
-
-Tue 15:13
-
-
-
-Antony Bahati uploaded an item
-
-Text
-
-main.h.txt
-
 #ifndef HOLBERTON_H_INCLUDED
-
 #define HOLBERTON_H_INCLUDED
 
-
-
 int _putchar(char);
-
 void print_alphabet(void);
-
-void print_alphabet_x10(void);
-
 int _islower(int c);
-
 int _isalpha(int c);
-
-int print_sign(int n);
-
-int _abs(int);
-
+int print_sign(int n); 
+int _abs(int); 
 int print_last_digit(int);
-
 void jack_bauer(void);
-
 void times_table(void);
-
 int add(int a, int b);
-
 void print_to_98(int n);
-
-void print_times_table(int n);
-
+void print_times_tables(int n); 
 void num(int r, int n);
 
-
-
-#endifYour storage is full. … You can't upload new files to Drive and may not be able to send or receive emails in Gmail.Learn more
-
-main.h.txt
-
-Yesterday
-
-Tue 15:13
-
-
-
-Antony Bahati uploaded an item
-
-Text
-
-main.h.txt
-
-#ifndef HOLBERTON_H_INCLUDED
-
-#define HOLBERTON_H_INCLUDED
-
-
-
-int _putchar(char);
-
-void print_alphabet(void);
-
-void print_alphabet_x10(void);
-
-int _islower(int c);
-
-int _isalpha(int c);
-
-int print_sign(int n);
-
-int _abs(int);
-
-int print_last_digit(int);
-
-void jack_bauer(void);
-
-void times_table(void);
-
-int add(int a, int b);
-
-void print_to_98(int n);
-
-void print_times_table(int n);
-
-void num(int r, int n);
-
-
-
-#endifYour storage is full. … You can't upload new files to Drive and may not be able to send or receive emails in Gmail.Learn more
-
-main.h.txt
-
-Yesterday
-
-Tue 15:13
-
-
-
-Antony Bahati uploaded an item
-
-Text
-
-main.h.txt
-
-#ifndef HOLBERTON_H_INCLUDED
-
-#define HOLBERTON_H_INCLUDED
-
-
-
-int _putchar(char);
-
-void print_alphabet(void);
-
-void print_alphabet_x10(void);
-
-int _islower(int c);
-
-int _isalpha(int c);
-
-int print_sign(int n);
-
-int _abs(int);
-
-int print_last_digit(int);
-
-void jack_bauer(void);
-
-void times_table(void);
-
-int add(int a, int b);
-
-void print_to_98(int n);
-
-void print_times_table(int n);
-
-void num(int r, int n);
-
-
-
-#endif
+#endif 
