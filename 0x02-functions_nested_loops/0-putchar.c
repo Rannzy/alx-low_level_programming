cb788d3fbd70cb86dcd6de25ade8c91a5de4599c
Rannzy <ernaran6@gmail.com>
2022-09-14 06:46:56 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,59 +1,21 @@
-Your storage is full. â€¦ You can't upload new files to Drive and may not be able to send or receive emails in Gmail.Learn more
-
-Task 0.txt
-
-Yesterday
-
-Tue 15:13
-
-
-
-Antony Bahati uploaded an item
-
-Text
-
-Task 0.txt
-
 #include "main.h"
 
-
-
 /**
-
- * main - print putchar
-
- * Return: always 0
-
- */
-
-
+*main - print putchar
+*Return: always 0
+*/
 
 int main(void)
-
 {
-
 	_putchar('_');
-
 	_putchar('p');
-
-	_putchar('u');
-
+	_putchar(Ã'u');
 	_putchar('t');
-
-	_putchar('c');
-
+	_putchar(Ã'c');
 	_putchar('h');
-
 	_putchar('a');
-
 	_putchar('r');
-
 	_putchar('\n');
 
-
-
 	return (0);
-
-
-
 }
