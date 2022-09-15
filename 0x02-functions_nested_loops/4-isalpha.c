16fb61f6f4c40abf2f472d7ab7e1fcbbd61c7365
Rannzy <ernaran6@gmail.com>
2022-09-15 04:03:04 -0700

    Letters

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _isalpha - shows if input is a letter.
+ * _isalpha - shows if input is a letter
  * @c: The character in ASCII code
- * Return: 1 for letters,otherwise 0.
+ *
+ * Return: 1 if a letter, 0 otherwise.
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122 || (c >= 65 && c <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
 	else
-	{
+	}
 		return (0);
 	}
 	_putchar('\n');
 }
-
 
