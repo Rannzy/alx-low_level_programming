9651aa25ecc9eaa5ed783683c79db1f4ac5ce2d5
Rannzy <ernaran6@gmail.com>
2022-09-15 02:54:05 -0700

    Absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute values
+ * @i: inputs number as an integer
+ *
+ * Return: absolute values
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
+
