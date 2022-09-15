dd7fb27c687cbd2c14aaa11bd9a9fbf2e80f1457
Rannzy <ernaran6@gmail.com>
2022-09-15 02:14:05 -0700

    Letters

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,8 +6,9 @@ include "main.h"
  *
  * Return: 1 for letters,otherwise 0.
  */
+int _isalpha(int c)
 {
-	if((c >=97 && c <= 122) || (c >= 65 && c <= 90))
+	if((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
