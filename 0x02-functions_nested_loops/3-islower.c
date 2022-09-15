9b1acb0dffd2373bb6cc729c6fb9fcd9d186709a
Rannzy <ernaran6@gmail.com>
2022-09-15 01:12:18 -0700

    Lowercase characters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
 *_islower - function that prints lowercase characters
-*@c: int used for tha argument function.
+*@c; int used for the argument function
 *Return: 0
 */
 int _islower(int c)
 {
-	if (c >= Ã'a' && c <= 'z') 
+	if (c >= Ã'a' && c <= 'z')
 	{
 		return (1);
 	}
-	else 
+	else
 		return (0);
 }
