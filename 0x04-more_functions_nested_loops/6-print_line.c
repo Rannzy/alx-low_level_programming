e9182e507ea6e855178bab637a3afad04e030c6f
Rannzy <ernaran6@gmail.com>
2022-09-16 11:52:09 -0700

    Straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ * @n: number of times the straight line is printed
+ * Return: none
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < nn; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
+
