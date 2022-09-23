3855376e4387747aed6fdfd4b7d3381a88c660c6
Rannzy <ernaran6@gmail.com>
2022-09-23 00:34:56 -0700

    Reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,11 +5,11 @@
  * @n: number of elements of the array
  * Return: none
  */
- void reverse_array(int *a, int n)
+void reverse_array(int *a, int n)
 {
 	int i, j, temp;
 
-	for (i = 0; i < n-1; i++)
+	for (i = 0; i < n - 1; i++)
 	{
 		for (j = i + 1; j > 0; j--)
 		{
